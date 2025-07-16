67a2d8e49897183e8d7e66f77a8f9dd3a2db3a0f
alinatatukova01 <alinaaa.t01@gmail.com>
2025-07-16 16:44:05 +0300

    greeting: change text, counting clicks, change colors

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,12 +1,27 @@
 import React from "react";
+import './Greeting.css'
 
 class Greeting extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      greeting: true,
+      count: 0,
+    }
+  }
+
+  changeText(){
+    this.setState({
+      greeting: !this.state.greeting,
+      count: this.state.count + 1
+    })
   }
 
   render() {
-    return <h1>Hello, {this.props.name}!</h1>;
+    const greeting = this.state.greeting ? "Hello" : "Buy";
+    const className = this.state.greeting ? "greenText" : "redText"
+
+    return <h1 onClick={() => {this.changeText()}} className={className}>{greeting}, {this.props.name}! Clicks: {this.state.count}</h1>;
   }
 }
 
